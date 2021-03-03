9e0dd4d52040436f7d0628f88c0ef0c171dde800
Wildes Piva <wildeshenrique8@gmail.com>
2021-03-02 21:45:10 -0300

    Passaram 15 testes

--- semana5/MyHolidays/myholidays/holidays.py
+++ semana5/MyHolidays/myholidays/holidays.py
@@ -4,19 +4,23 @@ from attr import dataclass
 
 class MyCalendar:
     def __init__(self, *args):
-        self.args = args
         self.datas = []
-        self.parse_dates()
+        self.parse_dates(args)
 
     
-    def parse_dates(self):
-        for item in self.args:
+    def parse_dates(self, dates_to_parse):
+        for item in dates_to_parse:
             if isinstance(item, date):
                 self.datas.append(item)
             elif isinstance(item, str):
                 try:
                     item = datetime.strptime(item, '%d/%m/%Y').date()
+                    self.datas.append(item)
                 except ValueError:
                     continue
-                self.datas.append(item)
+
+
+    def add_holiday(self, *args):
+        self.parse_dates(args)
+
             
