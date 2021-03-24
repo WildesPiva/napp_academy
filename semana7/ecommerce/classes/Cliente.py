3978952f57dd9c5c34fefb4a849a4f143eec167e
Wildes Piva <wildeshenrique8@gmail.com>
2021-03-24 13:54:54 -0300

    Refatorando 'Cliente'

--- semana7/ecommerce/classes/Cliente.py
+++ semana7/ecommerce/classes/Cliente.py
@@ -1,17 +1,9 @@
 class Cliente:
     def __init__(self, nome):
-        self._name = nome
-
-    @property
-    def nome(self):
-        return self._name
-
-    @nome.setter
-    def nome(self, value):
-        self._name = value
+        self.nome = nome
 
     def __str__(self):
-        return self._name
+        return self.nome
 
     def __repr__(self):
-        return 'Cliente => ' + self._name
+        return 'Cliente => ' + self.nome
