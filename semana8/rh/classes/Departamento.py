d1444145a39d1afba1b6f14ec513b2bd1be31375
Wildes Piva <wildeshenrique8@gmail.com>
2021-03-28 15:32:05 -0300

    Refatorando a classe Departamento

--- semana8/rh/classes/Departamento.py
+++ semana8/rh/classes/Departamento.py
@@ -2,24 +2,14 @@ from rh.classes.Colaborador import Colaborador
 
 
 class Departamento:
-    def __init__(self, nome_setor):
-        self._nome_setor = nome_setor
-        self._responsavel = None
+    def __init__(self, nome_setor, nome_responsavel, dia_responsavel, mes_responsavel, ano_responsavel):
+        self.nome = nome_setor
         self._colaboradores = []
-
-    @property
-    def nome(self):
-        return self._nome_setor
-
-    @nome.setter
-    def nome(self, value):
-        self._nome_setor = value
+        self.informar_responsavel(nome_responsavel, dia_responsavel, mes_responsavel, ano_responsavel)
 
     @property
     def responsavel(self):
-        if self._responsavel is None:
-            return None
-        return str(self._responsavel)
+        return self._responsavel
 
     @property
     def colaboradores(self):
@@ -27,6 +17,7 @@ class Departamento:
 
     def informar_responsavel(self, nome, dia, mes, ano):
         self._responsavel = Colaborador(nome, dia, mes, ano)
+        self.add_colaborador(nome, dia, mes, ano)     
 
     def add_colaborador(self, nome, dia, mes, ano):
         self._colaboradores.append(Colaborador(nome, dia, mes, ano))
@@ -35,11 +26,11 @@ class Departamento:
         lista = []
         for colaborador in self.colaboradores:
             if colaborador.aniversario_hoje():
-                lista.append((colaborador.nome, colaborador.aniversario))
+                lista.append((self.nome, colaborador.nome, colaborador.aniversario))
         return lista
 
     def __str__(self):
-        return self._nome_setor
+        return self.nome
 
     def __repr__(self):
-        return 'Departamento = ' + self._nome_setor
+        return 'Departamento = ' + self.nome
