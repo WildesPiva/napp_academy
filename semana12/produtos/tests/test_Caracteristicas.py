00c16d01970e8c82e53fb75d937535d532da6554
WildesPiva <wildeshenrique8@gmail.com>
2021-04-25 13:01:36 -0300

    Adicionando testes das novas caracteristicas

--- semana12/produtos/tests/test_Caracteristicas.py
+++ semana12/produtos/tests/test_Caracteristicas.py
@@ -1,7 +1,16 @@
-from produtos.classes.Produtos import Pepsi
-from produtos.classes.Caracteristicas import Caracteristicas
-from produtos.classes.Caracteristicas import Tamanho600ml
-from produtos.classes.Caracteristicas import Tamanho1litro
+from produtos.classes.Produtos import (
+    GuaranaAntartica,
+    Pepsi,
+    Dolly,
+)
+from produtos.classes.Caracteristicas import (
+    Caracteristicas,
+    Tamanho600ml,
+    Tamanho1litro,
+    Tamanho2litros,
+    Tamanho3litros,
+)
+
 import pytest
 
 
@@ -22,9 +31,31 @@ class TestCaracteristicas:
         assert isinstance(caracteristica, Tamanho1litro)
         assert objeto.operation() == msg
 
+    def test_Dolly_1_litro(self):
+        msg = 'Dolly tamanho: 2 litros.'
+        caracteristica = Tamanho2litros()
+        objeto = Dolly(caracteristica)
+        assert isinstance(caracteristica, Caracteristicas)
+        assert isinstance(caracteristica, Tamanho2litros)
+        assert objeto.operation() == msg
+
+    def test_GuaranaAntartica_1_litro(self):
+        msg = 'Guarana Antartica tamanho: 3 litros.'
+        caracteristica = Tamanho3litros()
+        objeto = GuaranaAntartica(caracteristica)
+        assert isinstance(caracteristica, Caracteristicas)
+        assert isinstance(caracteristica, Tamanho3litros)
+        assert objeto.operation() == msg
+
     def test_class_abstractClass(self):
         msg_erro = "Can't instantiate abstract class Caracteristicas "
         msg_erro = msg_erro + "with abstract methods operation_implementation"
         with pytest.raises(TypeError) as error:
             Caracteristicas()
+        assert str(error.value) == msg_erro
+
+    def test_class_abstractClass_error(self):
+        msg_erro = 'Class need to be instantiated with a Caracteristicas type'
+        with pytest.raises(TypeError) as error:
+            _ = Pepsi('teste')
         assert str(error.value) == msg_erro
