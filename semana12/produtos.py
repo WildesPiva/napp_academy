ce0cc074883bf51e8ec2cf67cb8e3a186791c855
wildespiva <wildespiva@pop-os.localdomain>
2021-04-25 12:58:35 -0300

    Adicionando as novas implementaçoes

--- semana12/produtos.py
+++ semana12/produtos.py
@@ -1,5 +1,15 @@
-from produtos.classes.Produtos import CocaCola, Pepsi
-from produtos.classes.Caracteristicas import Tamanho600ml, Tamanho1litro
+from produtos.classes.Produtos import (
+    GuaranaAntartica,
+    CocaCola,
+    Pepsi,
+    Dolly,
+)
+from produtos.classes.Caracteristicas import (
+    Tamanho600ml,
+    Tamanho1litro,
+    Tamanho2litros,
+    Tamanho3litros,
+)
 
 
 def client_code(produto):
@@ -21,4 +31,12 @@ if __name__ == "__main__":
 
     tamanho = Tamanho1litro()
     produto = Pepsi(tamanho)
+    client_code(produto)
+
+    tamanho = Tamanho2litros()
+    produto = Dolly(tamanho)
+    client_code(produto)
+
+    tamanho = Tamanho3litros()
+    produto = GuaranaAntartica(tamanho)
     client_code(produto)
