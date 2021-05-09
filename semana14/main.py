90c027c3774abdf11575672717e674fa1b7653a3
WildesPiva <wildeshenrique8@gmail.com>
2021-05-09 19:57:25 -0300

    Finalizando exercicios

--- semana14/main.py
+++ semana14/main.py
@@ -1,8 +1,19 @@
-from redes_sociais.classes.redes_sociais import facebook, linkedin
+from redes_sociais.classes.redes_sociais import (
+    facebook,
+    linkedin,
+    github,
+    instagram
+)
 
+redes = [
+    'facebook',
+    'linkedin',
+    'github',
+    'instagram'
+]
 
 if __name__ == "__main__":
-    profile_type = input("Qual perfil deseja criar: [facebook] ou [linkedin]")
+    profile_type = input(f"Qual perfil deseja criar: {redes}")
     profile = eval(profile_type.lower())()
     print("Criando perfil... ", type(profile).__name__)
     print("Perfil possui as sessões", profile.getSections())
